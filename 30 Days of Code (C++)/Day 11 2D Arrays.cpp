72befec707ecbb57ee34f7f68e136fed1deb10b9
Growlingfern <37191045+Growlingfern@users.noreply.github.com>
2018-03-10 14:48:21 +0000

    Hackerrank solution
    Day 11 of 30 days of code

---
+++ 30 Days of Code (C++)/Day 11 2D Arrays.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+    
+    int sum[16]; 
+    int h = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            sum[h] = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                    + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
+                    + arr[i+2][j+2];
+            h++;
+        }
+    }
+    std::sort(std::begin(sum), std::end(sum));
+    cout << sum[15];
+    
+    
+}  
+    
+
+
