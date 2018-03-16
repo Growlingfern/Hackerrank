2389b00e11db300eba3fc97a23c6231f19bc13d5
Growlingfern <37191045+Growlingfern@users.noreply.github.com>
2018-03-16 00:21:18 +0000

    Hackerrank Solutions
    Day 17 of 30 days of code

---
+++ 30 Days of Code (C++)/Day 17 More Exceptions.cpp
@@ -0,0 +1,10 @@
+//Write your code here
+class Calculator{
+ 
+    public: int power (int n, int p){
+        if(n < 0 || p < 0){
+                throw runtime_error("n and p should be non-negative");
+        }  
+    return pow(n, p);
+    }
+};         
